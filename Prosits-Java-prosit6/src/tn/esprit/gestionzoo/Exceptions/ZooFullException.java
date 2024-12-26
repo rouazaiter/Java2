e4da4b5f46fd38e74b2bc73e84d05ac3314b03ad
roua <131956549+rouazaiter@users.noreply.github.com>
2024-12-26 20:05:00 +0100

    Add files via upload

---
+++ Prosits-Java-prosit6/src/tn/esprit/gestionzoo/Exceptions/ZooFullException.java
@@ -0,0 +1,8 @@
+package tn.esprit.gestionzoo.Exceptions;
+
+public class ZooFullException extends Exception {
+    public  ZooFullException(){}
+    public ZooFullExeption(String msg){
+        super(msg);
+    }
+}
---
+++ Prosits-Java-prosit6/src/tn/esprit/gestionzoo/interfaces/Carnivore.java
@@ -0,0 +1,7 @@
+package tn.esprit.gestionzoo.interfaces;
+
+import tn.esprit.gestionzoo.entities.Aquatic;
+
+public interface Carnivore <T> {
+    void eatMeat(T meat){}
+}
